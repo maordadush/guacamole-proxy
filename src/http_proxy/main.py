3826f282124b4f4f0d0151c086132ba7c13fd872
raishoemi <raishoemi@gmail.com>
2021-09-26 18:10:52 +0300

    Wrap downloaded content in zip and send to client
    This change has 2 parts:
    1. Skip a hop of the file by giving the download URL to the middleware
       API, instead of sending it the file content (3 instead of 4 hops)
    2. Use the custom ZipFile to create the file header and send it, and
       then send the rest of the content to the client. This is because the
       response doesn't return until we send the first body chunk

--- src/http_proxy/main.py
+++ src/http_proxy/main.py
@@ -1,10 +1,13 @@
+import json
 import logging
 
 import requests
 from fastapi import FastAPI, Response, Request
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import StreamingResponse
 
 from config import EnvConfig
+from zip import ZipFile, ZIP_DEFLATED
 
 config = EnvConfig()
 
@@ -23,29 +26,41 @@ app.add_middleware(
 )
 
 
+@app.get('/test_stream')
+async def foo(request: Request):
+    async def generator():
+        # yield b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\\0\0\0\0\0\0\0\0\0\0'
+        # await asyncio.sleep(5) # Simulate long running task to retrieve data
+        yield 'data'
+    return StreamingResponse(generator())
+
+
 @app.get('/files')
 async def proxy_download(request: Request):
     original_uri = request.headers['x-original-uri']
     octet_stream_media_type = 'application/octet-stream'
-    guacamole_response = requests.get(
-        f'http://{config.guacamole_server_host}:{config.guacamole_server_port}{original_uri}', params=request.query_params)
-    if not guacamole_response.ok:
-        return Response(status_code=500)
 
-    original_file_content = guacamole_response.content
-    middleware_response = requests.post(
-        f'http://{config.middleware_api_host}:{config.middleware_api_port}/download', data=original_file_content)
-    if middleware_response.status_code == 204:
-        return Response(original_file_content, media_type=octet_stream_media_type)
+    def get_file_content_iterator():
+        # Wrapping file content in a zip in order to return any valid data ASAP (zip file header)
+        zip_stream = ZipFile(mode='w')
+        zip_header, zip_info = zip_stream.write_header('test', compress_type=ZIP_DEFLATED)
+        yield zip_header
 
-    elif middleware_response.status_code == 200:
-        modified_file_content = middleware_response.content
-        return Response(modified_file_content, media_type=octet_stream_media_type)
+        guacamole_request = {
+            'url': f'http://{config.guacamole_server_host}:{config.guacamole_server_port}{original_uri}',
+            'params': request.query_params._dict
+        }
+        middleware_response = requests.post(f'http://{config.middleware_api_host}:{config.middleware_api_port}/download',
+                                            data=json.dumps(guacamole_request), stream=True)
 
-    elif middleware_response.status_code == 403:
-        return Response(status_code=403)
+        if middleware_response.status_code == 200:
+            for chunk in zip_stream.write_content(middleware_response.iter_content(1024*32), zip_info):
+                yield chunk
+        for chunk in zip_stream._ZipFile__close():
+            yield chunk
+        
 
-    return Response(status_code=500)
+    return StreamingResponse(get_file_content_iterator(), media_type=octet_stream_media_type)
 
 
 @app.post('/files')
@@ -53,19 +68,22 @@ async def proxy_upload(request: Request):
     original_uri = request.headers['x-original-uri']
     octet_stream_media_type = 'application/octet-stream'
     guacamole_upload_uri = f'http://{config.guacamole_server_host}:{config.guacamole_server_port}{original_uri}'
-    
+
     original_file_content = await request.body()
-    middleware_response = requests.post(f'http://{config.middleware_api_host}:{config.middleware_api_port}/upload', data=original_file_content)
+    middleware_response = requests.post(
+        f'http://{config.middleware_api_host}:{config.middleware_api_port}/upload', data=original_file_content)
 
     if middleware_response.status_code == 204:
-        guacamole_response = requests.post(guacamole_upload_uri, params=request.query_params, data=original_file_content)
+        guacamole_response = requests.post(
+            guacamole_upload_uri, params=request.query_params, data=original_file_content)
         return Response(guacamole_response.text, status_code=guacamole_response.status_code, media_type=octet_stream_media_type)
 
     elif middleware_response.status_code == 200:
         modified_file_content = middleware_response.content
-        guacamole_response = requests.post(guacamole_upload_uri, params=request.query_params, data=modified_file_content)
+        guacamole_response = requests.post(
+            guacamole_upload_uri, params=request.query_params, data=modified_file_content)
         return Response(guacamole_response.text, status_code=guacamole_response.status_code, media_type=octet_stream_media_type)
-    
+
     elif middleware_response.status_code == 403:
         return Response(status_code=403)
 
